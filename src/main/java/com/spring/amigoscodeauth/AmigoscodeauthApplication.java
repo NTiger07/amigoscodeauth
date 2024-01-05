272f2363aad8e4698cb174da42181cf60666f684
NTiger07 <falaleru@gmail.com>
2024-01-05 20:40:10 +0100

    Initial Commit

---
+++ src/main/java/com/spring/amigoscodeauth/AmigoscodeauthApplication.java
@@ -0,0 +1,13 @@
+package com.spring.amigoscodeauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodeauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoscodeauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/amigoscodeauth/AmigoscodeauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.amigoscodeauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodeauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
